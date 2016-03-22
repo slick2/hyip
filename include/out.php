444b63ee3e03da2cba469d7255024b5b92371342
Александр Корж <ak@rocketstation.ru>
2016-03-22 12:57:51 +0300

    Small fix out

--- include/out.php
+++ include/out.php
@@ -4,27 +4,25 @@ $mysqli = Database::getInstance();
 $holidays = array(0, 6);
 $message = "";
 $percent = in_array(date("w", strtotime("today")), $holidays) ? HOLIDAY_PERCENT : BUSINESS_PERCENT;
-$qcash = $mysqli->query("SELECT id,cash*$percent AS cash FROM hyip_cash")->fetchAll();
-foreach ($qcash as $row) 
-{
-    $sum = $row['cash'] * $percent;
-    $sqlcur = "select hp.currency as currency, hu.email as email, acc.account as account, hsys.name as name from  hyip_payaccounts hp 
+$qcash = $mysqli->query("select hc.id as id, hc.cash*$percent as cash, hp.currency as currency, hu.email as email, acc.account as account, hsys.name as name 
+from  hyip_payaccounts hp 
 inner join hyip_cash hc ON (hc.payaccount_id=hp.id)
 inner join hyip_users hu ON (hc.user_id=hu.id)
 inner join hyip_payaccounts acc ON (acc.id=hc.payaccount_id)
 inner join hyip_paysystems hsys ON (hsys.id=acc.paysystem_id)
-where acc.account is not null AND hc.id={$row['id']}";
-
-    $data = $mysqli->query($sqlcur)->fetchSingleRow();
-    if ($data['account'] != NULL) 
+where acc.account is not null")->fetchAll();
+foreach ($qcash as $row) 
+{
+    $sum = $row['cash'] * $percent;
+    if ($row['account'] != NULL) 
     {
         $mysqli->query('START TRANSACTION;');
         $mysqli->query("UPDATE hyip_cash SET outs=outs+1 WHERE id='" . $row['id'] . "'");
         $mysqli->query("INSERT INTO hyip_orders (cash_id,operation,`sum`,code) VALUES (" . $row['id'] . ",1,$sum,0)");
         $mysqli->query('COMMIT');
-        $message .= "Вывод суммы: " . $sum . " " . strtoupper($data['currency']) . "\n";
-        $message .= "Пользователю: " . $data['email'] . "\n";
-        $message .= "На кошелек: " . $data['name'] . " " . $data['account'] . "\n";
+        $message .= "Вывод суммы: " . $sum . " " . strtoupper($row['currency']) . "\n";
+        $message .= "Пользователю: " . $row['email'] . "\n";
+        $message .= "На кошелек: " . $row['name'] . " " . $row['account'] . "\n";
     }
 }
 echo str_replace("\n", "<br>", $message);
