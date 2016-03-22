7f55c1235e5cad1fc376faa1becd806d1809ae91
Александр Корж <ak@rocketstation.ru>
2016-03-22 16:07:03 +0300

    Refactor out

--- include/out.php
+++ include/out.php
@@ -4,20 +4,20 @@ $mysqli = Database::getInstance();
 $holidays = array(0, 6);
 $message = "";
 $percent = in_array(date("w", strtotime("today")), $holidays) ? HOLIDAY_PERCENT : BUSINESS_PERCENT;
-$qcash = $mysqli->query("select hc.id as id, hc.cash*$percent as cash, hp.currency as currency, hu.email as email, acc.account as account, hsys.name as name 
+$qcash = $mysqli->query("select hc.date as date, hc.id as id, hc.cash*$percent as cash, hp.currency as currency, hu.email as email, acc.account as account, hsys.name as name 
 from  hyip_payaccounts hp 
 inner join hyip_cash hc ON (hc.payaccount_id=hp.id)
 inner join hyip_users hu ON (hc.user_id=hu.id)
 inner join hyip_payaccounts acc ON (acc.id=hc.payaccount_id)
 inner join hyip_paysystems hsys ON (hsys.id=acc.paysystem_id)
-where acc.account is not null")->fetchAll();
+where acc.account is not null and DATE(hc.date) != CURDATE()  LIMIT 1000")->fetchAll();
 foreach ($qcash as $row) 
 {
     $sum = $row['cash'] * $percent;
     if ($row['account'] != NULL) 
     {
         $mysqli->query('START TRANSACTION;');
-        $mysqli->query("UPDATE hyip_cash SET outs=outs+1 WHERE id='" . $row['id'] . "'");
+        $mysqli->query("UPDATE hyip_cash SET outs=outs+1, last_out='$now' WHERE id={$row['id']}");
         $mysqli->query("INSERT INTO hyip_orders (cash_id,operation,`sum`,code) VALUES (" . $row['id'] . ",1,$sum,0)");
         $mysqli->query('COMMIT');
         $message .= "Вывод суммы: " . $sum . " " . strtoupper($row['currency']) . "\n";
