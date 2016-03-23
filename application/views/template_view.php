3f1cbcd0776b1c86d64cc9286765fa3de86e7593
Roswell Anthony <Roswell Anthony>
2016-03-23 17:51:52 +0300

    Redesing project

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -4,49 +4,80 @@
         <meta charset="utf-8">
             <title>Пирамида</title>
             <link rel="icon" type="/image/png" href="ic1.png" sizes="16x16"/>
-            <link rel="stylesheet" href="/css/style.css" type='text/css' media='all'/>
+            <!--link rel="stylesheet" href="/css/style.css" type='text/css' media='all'/-->
             <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css"/>
             <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'/>
             <link rel="stylesheet" type="text/css" href="/css/custom.css"/>
+            <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+            <link rel="stylesheet" type="text/css" href="/css/lk-style.css">
     </head>
     <body>
-        <div class="head-lk">
-            <div class="logo"></div>
-            <div class="main_nav-lk">
-                <ul class="navigation-lk">
-                    <li><a href="/"><i class="fa fa-home fa-fw"></i>ГЛАВНАЯ</a></li>
-                    <li><a href="/o-nas/">О НАС</a></li>
-                    <li><a href="/investoram/">ИНВЕСТОРАМ</a></li>
-                    <li><a href="/pravila-kompanii/">ПРАВИЛА</a></li>
-                    <li><a href="/vopros-otvet/">ВОПРОС-ОТВЕТ</a></li>
-                    <li><a href="/contact/">КОНТАКТЫ</a></li>
-                </ul>
-            </div>
-            <span><a href="private"><i class="fa fa-user"></i> <?php echo Session::get('name'); ?></a>
-                <a class="btn-lk" href="/logout">Выйти <i class="fa fa-sign-out"></i></a>
-                <i class="fa fa-clock-o" style="margin-left: 20px; padding-right: 5px;"></i><span id="time"></span>
-            </span>
+<div class="container-fluid">
+        <div class="row">
+                <nav class="navbar navbar-default main-nav" role="navigation">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="col-md-2 col-md-offset-2">
+                    <div class="navbar-header">
+                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                          </button>
+                          <div class="col-md-offset-3">
+                      <a class="navbar-brand company-logo"  href="#"></a>
+                          </div>
+                    </div>
+                    </div>
+
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="col-md-7">
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                      <ul class="nav navbar-nav">
+                        <li><a href="#"><span class="glyphicon glyphicon-home"></span>ГЛАВНАЯ</a></li>
+                        <li><a href="#">О НАС</a></li>
+                        <li><a href="#">ИНВЕСТОРАМ</a></li>
+                        <li><a href="#">ПРАВИЛА</a></li>
+                        <li><a href="#">ВОПРОС-ОТВЕТ</a></li>
+                        <li><a href="#">КОНТАКТЫ</a></li>
+                      </ul>
+                    </div><!-- /.navbar-collapse -->
+                    </div>
+                </nav>
         </div>
-        <div class="container-fluid cabinet-wrap">
-            <div class="container">
-                <div class="left-menu">
-                    <h2><i class="fa fa-user"></i> <?php echo Session::get('name'); ?></h2>
-                    <ul class="left-menue-lk">
-                        <?php if(Session::get('role') === 'user'): ?>
-                        <li><a href="/private"><i class="fa fa-server"></i>Кабинет</a></li>
-                        <li><a href="/deposits/add"><i class="fa fa-university"></i>Создать депозит</a></li>
-                        <li><a href="/deposits"><i class="fa fa-credit-card"></i>Мои депозиты</a></li>
-                        <li><a href="/referral"><i class="fa fa-users"></i>Партнерская программа</a></li>
-                        <li><a><i class="fa fa-comments-o"></i>Рекламные материалы</a></li>
-                        <li><a href="/settings"><i class="fa fa-cogs"></i>Настройки</a></li>
-                        <?php else: ?>
-                        <li><a href="/admin"><i class="fa fa-cogs"></i>Администрирование</a></li>
-                        <?php endif ?>
-                    </ul>
-                </div>
-                <?php include 'application/views/' . $content_view; ?>
+
+        <div class="row">
+            <div class="col-md-2 col-md-offset-2  left-menu">
+                <ul class="nav nav-pills nav-stacked">
+                <?php if(Session::get('role') === 'user'): ?>
+                    <li><a href="/private"><span class="glyphicon glyphicon-user"></span>Кабинет</a></li>
+                    <li><a href="/deposits"><span class="glyphicon glyphicon-credit-card"></span>Инвестиции</a></li>
+                    <li><a href="/deposits/add"><span class="glyphicon glyphicon-briefcase"></span>Внести депозит</a></li>
+                    <li><a href="/referral"><span class="glyphicon glyphicon-thumbs-up"></span>Рефералы</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-bullhorn"></span>Рекламные материалы</a></li>
+                    <li><a href="/settings"><span class="glyphicon glyphicon-cog"></span>Настройки</a></li>
+                    <li><a href="/history"><span class="glyphicon glyphicon-th-list"></span>История</a></li>
+                    <li><a href="/logout"><span class="glyphicon glyphicon-off"></span>Выход</a></li>
+                    <?php else: ?>
+                    <li><a href="/admin"><i class="fa fa-cogs"></i>Администрирование</a></li>
+                    <?php endif ?>
+                </ul> 
+            </div>
+            
+            <div class="col-md-6">
+            <div class="container-fluid right-block">
+                <div class="cold-md-2">
+                <h2><i class="fa fa-user"></i> <?php echo Session::get('name'); ?></h2>
+                    <div class="row" style="border-top:1px solid #d8d8d8; border-bottom:1px solid #d8d8d8">
+                        <div class=" ref-link"><p>Реферальная ссылка<p></div><div class="col-md-5"><h5><?php echo "http://". $_SERVER['SERVER_NAME']."/register?ref=".Session::get('id');?></h5></div>
+                    </div>
+                    
+            </div>
+            <?php include 'application/views/' . $content_view; ?>
             </div>
         </div>
+</div>
         <script src="/js/jquery-2.2.1.min.js"></script>
         <script src="/js/readonly.js"></script>
         <script src="/js/activate.js"></script>
