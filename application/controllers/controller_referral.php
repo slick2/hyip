273fde5893ae9fe0d2f738b1d2564aa63a7ef0fc
Александр Корж <ak@rocketstation.ru>
2016-04-04 14:48:09 +0300

    reflinks page add and out fix

--- application/controllers/controller_referral.php
+++ application/controllers/controller_referral.php
@@ -6,6 +6,21 @@ class Controller_Referral extends Controller
         parent::__construct();
         $this->model = new Model_Referral();        
     }
+    
+    function action_links()
+    {
+        if (Session::get('email') !== false)
+        {
+            $data['text'] = $this->model->get_messages('ref');
+            $this->view->generate('reflinks_view.php', 'template_view.php', $data);
+        }
+        else
+        {
+            Session::destroy();
+            header("Location:/auth");
+        }
+    }
+            
     function action_index()
     {
         if (Session::get('email') !== false)
