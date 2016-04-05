5507541526f63a960a455171e6af9c3143ba004a
Roswell Anthony <Roswell Anthony>
2016-04-05 11:17:17 +0300

    add page and banners

--- application/views/reflinks_view.php
+++ application/views/reflinks_view.php
@@ -1,1 +1,31 @@
+<div class="container-fluid new-depos">
+    <div class="col-xs-12">
+        <div class="row">
+        <img src="/img/banners/big.gif" class="center-block"/>
+        </div>
+    </div>
+        <textarea disabled="disabled" width="100%" class="col-xs-12 well well_well-banner"><a href="<?php echo "https://". $_SERVER['SERVER_NAME']."/auth/register?ref=".Session::get('id');?>"><img src="https://pa.itinvestproject.com/img/banners/big.gif" /></a></textarea>
+
+</div>
+
+<div class="container-fluid new-depos">
+    <div class="col-xs-12">
+        <div class="row">
+        <img src="/img/banners/small.gif" class="center-block"/>
+        </div>
+    </div>
+        <textarea disabled="disabled" width="100%" class="col-xs-12 well well_well-banner"><a href="<?php echo "https://". $_SERVER['SERVER_NAME']."/auth/register?ref=".Session::get('id');?>"><img src="https://pa.itinvestproject.com/img/banners/small.gif" /></a></textarea>
+
+</div>
+
+<div class="container-fluid new-depos">
+    <div class="col-xs-12">
+        <div class="row">
+        <img src="/img/banners/vertical.gif" class="center-block"/>
+        </div>
+    </div>
+        <textarea disabled="disabled" width="100%" class="col-xs-12 well well_well-banner"><a href="<?php echo "https://". $_SERVER['SERVER_NAME']."/auth/register?ref=".Session::get('id');?>"><img src="https://pa.itinvestproject.com/img/banners/vertical.gif" /></a></textarea>
+
+</div>
 <?php
+
