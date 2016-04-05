7da60a69c065295ffcfaea2d293e9d5e35b4b6d5
Roswell Anthony <Roswell Anthony>
2016-04-05 12:59:07 +0300

    smaller fix

--- application/views/reflinks_view.php
+++ application/views/reflinks_view.php
@@ -1,6 +1,26 @@
 <div class="container-fluid new-depos">
     <div class="col-xs-12">
         <div class="row">
+        <img src="/img/banners/smaller.gif" class="center-block"/>
+        </div>
+    </div>
+        <textarea disabled="disabled" width="100%" class="col-xs-12 well well_well-banner"><a href="<?php echo "https://". $_SERVER['SERVER_NAME']."/auth/register?ref=".Session::get('id');?>"><img src="https://pa.itinvestproject.com/img/banners/smaller.gif" /></a></textarea>
+
+</div>
+
+<div class="container-fluid new-depos">
+    <div class="col-xs-12">
+        <div class="row">
+        <img src="/img/banners/square.gif" class="center-block"/>
+        </div>
+    </div>
+        <textarea disabled="disabled" width="100%" class="col-xs-12 well well_well-banner"><a href="<?php echo "https://". $_SERVER['SERVER_NAME']."/auth/register?ref=".Session::get('id');?>"><img src="https://pa.itinvestproject.com/img/banners/square.gif" /></a></textarea>
+
+</div>
+
+<div class="container-fluid new-depos">
+    <div class="col-xs-12">
+        <div class="row">
         <img src="/img/banners/big.gif" class="center-block"/>
         </div>
     </div>
